bd5e32ce45965472349389a0e7b0688e8fe3ca8d
kateheese <kateheese@gmail.com>
2016-01-03 15:52:21 -0700

    users can add ingredients to kitchen or select from suggestions

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,2 +1,34 @@
 app.controller('KitchenController', ['$scope','$http', function($scope, $http) {
+  $scope.kitchen = [];
+  $scope.suggestions = [
+    'salt',
+    'pepper',
+    'garlic',
+    'olive oil',
+    'eggs',
+    'milk',
+    'rice',
+    'pasta',
+    'butter',
+    'sugar',
+    'flour',
+    'vanilla',
+    'corn starch'
+  ];
+
+  $scope.addSuggestion = function() {
+    $scope.kitchen.push(this.item);
+    var index = $scope.suggestions.indexOf(this.item);
+    $scope.suggestions.splice(index, 1);
+  }
+
+  $scope.addIngredient = function() {
+    $scope.kitchen.push($scope.ingredient);
+    $scope.ingredient = '';
+  }
+
+  $scope.removeIngredient = function() {
+    var index = $scope.kitchen.indexOf(this.item);
+    $scope.kitchen.splice(index, 1);
+  }
 }])
